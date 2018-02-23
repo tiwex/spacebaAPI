5bd1ab99f1a64824e07e078f5a32a9f6b884aea9
taiwo akinseye <taiwo.akinseye@gmail.com>
2018-02-23 18:10:47 +0100

    added algolia driver and scot package,synced space model

--- app/Space.php
+++ app/Space.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Space extends Model
 {
     //
+     use Searchable;
 }
--- config/app.php
+++ config/app.php
@@ -163,10 +163,13 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        
+		Laravel\Scout\ScoutServiceProvider::class,
 
         /*
          * Application Service Providers...
